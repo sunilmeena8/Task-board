addcaf65081020b7369ea195a51e2f7431eb054a
wrangler8 <sunil98meena@gmail.com>
2020-03-20 20:23:12 +0530

    authentication done

--- projectboard/accounts/views.py
+++ projectboard/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 
 # Create your views here.
@@ -12,7 +12,19 @@ def homeview(request):
 	return(render(request,'accounts/home.html',{}))
 
 def login_req(request):
-	pass
+	if(request.method=='POST'):
+		form = AuthenticationForm(request,data=request.POST)
+		if(form.is_valid()):
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username,password=password)
+			if(user is not None):
+				login(request,user)
+				return(render(request,'accounts/home.html',{}))
+	else:
+		form = AuthenticationForm()
+
+	return(render(request,'accounts/register.html',{'form':form}))
 
 def register(request):
 	if(request.method=='POST'):
