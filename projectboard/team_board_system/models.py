a9be64d17ab6d370a048b42c605f112af4eeb25f
wrangler8 <sunil98meena@gmail.com>
2020-03-22 10:19:01 +0530

    models of team baord done

--- projectboard/team_board_system/models.py
+++ projectboard/team_board_system/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TeamBoard(models.Model):
+	user = models.ManyToManyField(User)
+	title = models.CharField(max_length=100)
+
+class TeamBoardList(models.Model):
+	board = models.ForeignKey(TeamBoard,on_delete=models.CASCADE)
+	title = models.CharField(max_length = 100)
+
+class TeamBoardListCard(models.Model):
+	list = models.ForeignKey(TeamBoardList,on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	due_date = models.DateTimeField()
+	attachment = models.FileField()
+	archived = models.BooleanField()
+	
+
